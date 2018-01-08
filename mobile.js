01d9bf65c8aedff41e1c3c5043d780409f311ca8
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-01-08 11:00:17 -0600

    fixed mobile menu resize problem

--- mobile.js
+++ mobile.js
@@ -1,27 +1,34 @@
-const width = window.innerWidth;
+
 const nav = document.querySelector('.nav');
 const img = document.createElement('img');
+const navDiv = document.createElement('div');
+img.src = 'images/mobile-menu.png';
+img.className = 'mobile-button open';
+let mobileNav = nav.innerHTML;
+const li = nav.querySelector('.active');
 
-let mobileNav;
-
-addResponsiveNav = (width) => {
-  img.src = 'images/mobile-menu.png';
-  img.className = 'mobile-button open';
-  let mobileNav;
-  if(width < 568) {
-    mobileNav = nav.innerHTML;
+responsive = width => {
+  if(width <= 568) {
     nav.innerHTML = '';
     nav.appendChild(img);
-  }
-  return mobileNav
-};
+    navDiv.className = 'mobile-menu';
+    navDiv.innerHTML = mobileNav;
 
-mobileNav = addResponsiveNav(width);
-console.log(mobileNav);
-const navDiv = document.createElement('div');
-navDiv.className = 'mobile-menu';
-navDiv.innerHTML = mobileNav;
-const li = navDiv.querySelector('.active');
+
+
+  } else if(width > 568) {
+    console.log("Not Here!");
+    nav.innerHTML = mobileNav;
+  }
+}
+
+const width = window.innerWidth;
+responsive(width);
+
+window.addEventListener('resize', (e) => {
+  const width = window.innerWidth;
+  responsive(width);
+});
 
 img.addEventListener('click', (e) => {
   for (let i = 0; i < img.classList.length; i++) {
@@ -31,7 +38,6 @@ img.addEventListener('click', (e) => {
       img.className = 'mobile-button close';
       nav.appendChild(navDiv);
       li.style.backgroundColor = 'black';
-      console.log(li);
     } else if (img.classList[i] === 'close') {
       img.classList.remove = 'close';
       img.src = 'images/mobile-menu.png';
